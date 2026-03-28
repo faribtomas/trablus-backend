62770181878dbdaedaedda4df6bb76e69ce4addb
farib tomas <tomasfarib@gmail.com>
2026-03-28 17:17:33 -0300

    Actualizando a mysql2 para nube

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const app = express();
 app.use(cors());
